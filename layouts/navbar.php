94b82484f9805d83b869eee665255310c4519277
FloresEdd <e.flores27nov@hotmail.com>
2023-11-15 22:31:10 -0600

    add navbar.php

---
+++ layouts/navbar.php
@@ -0,0 +1,6 @@
+<nav align="center">
+    <a href="vender.php">Vender</a>
+    <a href="informe.php">Informes</a>
+    <a href="registro.php">Registro de productos</a>
+    <img src="../logo.jpeg">
+</nav>
