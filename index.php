394a95139ecd0c13d417d83014659cc8c61f4f3c
FloresEdd <e.flores27nov@hotmail.com>
2023-11-15 21:54:35 -0600

    index.php modified

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ens
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La principal </title>
+</head>
+<body>
+    
+</body>
+</html>
