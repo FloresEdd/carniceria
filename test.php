5712b06a12d15f33aaa279e36426b195c49d2e59
FloresEdd <e.flores27nov@hotmail.com>
2023-11-26 21:17:17 -0600

    test.php updated

--- test.php
+++ test.php
@@ -1,25 +1,53 @@
 <?php
-require "DataBase/conexion.php";
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['imagen'])) {
-    $nombreImagen = $_FILES['imagen']['name'];
-    $tipoImagen = $_FILES['imagen']['type'];
-    $tamanoImagen = $_FILES['imagen']['size'];
-    $tempImagen = $_FILES['imagen']['tmp_name'];
+// Configuración de la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "carniceria";
 
-    //verify if the file is an image
-    if($tipoImagen == "image/jpeg" || $tipoImagen == "image/png") {
-        // save the image on db
-        $query = "INSERT INTO imagenes (nombre, tipo, tamano, imagen) VALUES (?, ?, ?, ?)";
-        $statement = $conn->prepare($query);
-        $statement->bind_param("ssis", $nombreImagen, $tipoImagen, $tamanoImagen, $tempImagen);
-        $statement->execute();
-        $statement->close();
-        
-        echo "Imagen subida exitosamente";
-    } else {
-        echo "Sólo se permiten archivos JPEG y PNG";
-    }
+// Conexión a la base de datos
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Error al conectar con la base de datos: " . $conn->connect_error);
 }
+
+// Subir imagen
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nombreImagen = $_FILES["imagen"]["name"];
+    $tipoImagen = $_FILES["imagen"]["type"];
+    $tamanoImagen = $_FILES["imagen"]["size"];
+    $rutaImagen = $_FILES["imagen"]["tmp_name"];
+
+    // Leer los datos de la imagen
+    $imagen = file_get_contents($rutaImagen);
+    
+    // Preparar consulta SQL para insertar la imagen en la base de datos
+    $query = "INSERT INTO imagenes (nombre, tipo, tamano, imagen) VALUES (?, ?, ?, ?)";
+    $statement = $conn->prepare($query);
+    $statement->bind_param("ssis", $nombreImagen, $tipoImagen, $tamanoImagen, $imagen);
+    $statement->execute();
+    
+    echo "La imagen se ha subido correctamente.";
+}
+
+// Obtener todas las imágenes de la base de datos
+$query = "SELECT nombre, tipo, imagen FROM imagenes";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $nombreImagen = $row["nombre"];
+        $tipoImagen = $row["tipo"];
+        $imagen = $row["imagen"];
+
+        // Mostrar la imagen en la página web
+        echo "<img src='data:$tipoImagen;base64," . base64_encode($imagen) . "' alt='$nombreImagen'>";
+    }
+} else {
+    echo "No se encontraron imágenes en la base de datos.";
+}
+
+$conn->close();
 ?>
 <form method="POST" enctype="multipart/form-data">
     <input type="file" name="imagen" accept="image/jpeg, image/png" required>
