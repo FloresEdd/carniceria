754a37935ebfe2ab141dc870128af8ebeb7c5921
FloresEdd <e.flores27nov@hotmail.com>
2023-12-10 21:59:53 -0600

    funcionally index, js is missing

--- index.php
+++ index.php
@@ -6,74 +6,45 @@ if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
 
-// Consultar todos los productos
-$sql = "SELECT * FROM productos";
+// Preparar la consulta SQL para seleccionar los datos
+$sql = "SELECT name, precio, imagen FROM productos";
 $result = $conn->query($sql);
 
-// Consultar todas las imágenes
-$sql2 = "SELECT * FROM imagenes";
-$result2 = $conn->query($sql2);
-
-// Verificar si hay productos
-if ($result && $result2->num_rows > 0) {
+// Verificar si hay resultados
+if ($result->num_rows > 0) {
     echo "<table>";
-    echo "<tr>
-        <th>Nombre del producto</th>
-        <th>Imagen</th>
-        <th>Precio</th>
-    </tr>";
+    echo "<tr><th>Nombre</th><th>Precio</th><th>Imagen</th></tr>";
 
-    // Obtener todas las imágenes
-    $images = array();
-    while ($row2 = $result2->fetch_assoc()) {
-        $images[$row2["nombre"]] = "data:" . $row2["tipo"] . ";base64," . base64_encode($row2["imagen"]);
-    }
-
-    // Iterar todos los productos
+    // Recorrer los resultados y mostrar los datos en una tabla
     while ($row = $result->fetch_assoc()) {
-        $name = $row["name"];
+        $nombre = $row["name"];
         $precio = $row["precio"];
+        $imagen = $row["imagen"];
 
-        // Encontrar la imagen correspondiente para el producto
-        $imagen = isset($images[$name]) ? $images[$name] : "";
-
-        // Mostrar los productos en la tabla
-        echo "<tr>
-            <td>$name</td>
-            <td><img src='$imagen' alt='$name'></td>
-            <td>$precio</td>
-        </tr>";
+        echo "<tr>";
+        echo "<td>$nombre</td>";
+        echo "<td>$precio</td>";
+        echo "<td><img widht='100px' height='100px'src='data:image/PNG;base64," . base64_encode($imagen) . "' alt=''></td>";
+        echo "</tr>";
     }
 
     echo "</table>";
 } else {
-    echo "No hay productos";
+    echo "No se encontraron resultados";
 }
 
+// Cerrar la conexión
 $conn->close();
 ?>
 <!DOCTYPE html>
 <html lang="es">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
 <title>La principal</title>
+
 </head>
 
 <body>
 
 </body>
-
-</html>
-<!DOCTYPE html>
-<html lang="es">
-<meta charset=" UTF-8">
-
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>La principal </title>
-</head>
-
-<body>
-
-</body>
-
-</html>
